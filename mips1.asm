0fd4f071a5982f8e41e4a07a453c56eb950e3f7b
Yoonchan <yoon3971@gmail.com>
2017-03-30 19:16:49 -0400

    ubuntu

--- mips1.asm
+++ mips1.asm
@@ -39,7 +39,8 @@ main:
 								#First argument is the input number and saved at $a0
 	addi $a1, $zero, 0					#Second argument is zero and saved at $a1
 	jal binary_palindrome
-	beq $v0, 1, exit_true
+	addi $t1, $v0, 0
+	#beq $t1, 1, exit_true
 	
 	#test for checking return value
 	#currently return 0 from binary_palindrome
@@ -52,28 +53,39 @@ main:
 .globl binary_palindrome					#declare the subroutine to be global
 binary_palindrome:						#it has two arguments $a0, $a1
 	
-	bgt $a1, 16, true_bp
+	beq $a1, 16, true_bp
 	addi $s0, $a0, 0					#save $a0 to $s0
-	addi $s1, $a1, 0					#save $a1 to $s1
+	addi $t1, $a1, 0					#save $a1 to $s1
 	
 	 
 	######################################################################################### 
-	sub $s4, $31, $s1					# 31-i
-	srlv $s2, $s0, $s4					#shift right (31-i)th
-	sllv $s3, $s0, $s4					#shift left (31-i)
-	#addi $s4, $s4, 1
-	srlv $s3, $s3, $s4					#shift right back (31-i)
-	#sub $s3, $s3, 1
-	# i think this part is wrong
-	# need to find another way
-	#
+	#################################################################################
+	addi $s1, $s0, 0			##t0 == t1
+	addi $t0, $zero, 4
+	#sub $t1, $t4, 10
+						# 31-i
+	#srlv $s2, $t0, $t1					#shift right (31-i)th
+	addi $t2, $zero, 31
+	#sllv $s3, $a0, $t1					#shift left (31-i)
+	sllv $s2, $s0, $t0
+	addi $s3, $s2, 0
+	srlv $s2, $s3, $t2
+	
+	#addi $t5, $zero, 24
+	sub $t3, $31, $t0
+	#addi $s6, $t1, 0
+	sllv $s4, $s1, $t3 
+	addi $s5, $s4, 0
+	srlv $s4, $s5, $t2
+
+	
+	#################################################################################
 	#########################################################################################
 	
 	#addi $a0, $s3, 0
 	
-	bne $s2, $s3, false_bp					# i-th digit != (31-1)th digit, go to exit_bp
+	bne $s2, $s4, false_bp					# i-th digit != (31-1)th digit, go to exit_bp
 	addi $a1, $a1, 1					#need to increase i
-	
 	jal binary_palindrome
 
 true_bp:
@@ -85,7 +97,7 @@ false_bp:
 	
 	
 exit_true:
-	j last
+	j exit
 exit:
 	addi $t0, $v0, 0
 	addi $t1, $a0, 0					#save return value
@@ -100,5 +112,3 @@ exit:
 	li $v0, 1
 	syscall
 
-last:
-	
