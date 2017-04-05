62138e4b1989a1b150f8e3e115cbc88a982c0461
Yoonchan Jeon <yoon3971@gmail.com>
2017-04-04 20:32:53 -0400

    4/4

--- mips1.asm
+++ mips1.asm
@@ -27,12 +27,12 @@ main:
 	
 	li $v0, 5
 	syscall
-	addi $t1, $v0, 0
+	addi $s1, $v0, 0
 	
 	addi $a0, $t0, 0
-	addi $a1, $t1, 0
+	addi $a1, $s1, 0
 	jal opt_multiply
-	addi $t1, $v0, 0
+	addi $s7, $v0, 0
 	j exit
 
 .globl opt_multiply					#declare the subroutine to be global
@@ -41,40 +41,64 @@ opt_multiply:
 	addi $s1, $a1, 0				# it will be the product
 	addi $t1, $a1, 0 				# checking original multiplier
 	
+	addi $t9, $zero, 1
+	addi $t8, $s1, 0
+	
+loop2:
+	addi $t9, $t9, 1
+	srl $t8, $t8, 1
+	bne $t8, $zero, loop2
+	
 loop:	
 	beq $t1, $zero, ExitReturn
 	andi $t2, $s1, 1
 	beq $t2, $zero, zeroTerm
 	#case last bit is 1
 	addi $t4, $s0, 0
-	sll $t5, $t4, 16
-	addu $t4, $t5, $s1
-	addu $s1, $s1, $t4
+	
+
+	######################this one should be upper half of multiplier 
+	sllv $t5, $t4, $t9
+	##################33
+	addi $t4, $zero, 0
+	
+	add $t4, $t5, $s1
+	addi $s1, $t4, 0
+	
+	srl $s2, $s1, 1
+	addi $s1, $zero, 0
+	addi $s1, $s2, 0
+	#####################
+	#need to check overflow
+	#if there was overflow before shifting then add 2147483648
+	#addi $s1, $s2, 2147483648
 	srl $t3, $t1, 1
+	addi $t1, $zero, 0
 	addi $t1, $t3, 0
 	j loop
 	
 zeroTerm:
 	 srl $s2, $s1, 1
+	 addi $s1, $zero, 0
 	 addi $s1, $s2, 0
 	 srl $t3, $t1, 1
+	 addi $t1, $zero, 0
 	 addi $t1, $t3, 0
 	 j loop
 	
 ExitReturn:
+	addi $s4, $zero, 0
 	srl $s4, $s2, 1
 	addi $v0, $s4, 0
 	jr $ra
-	
-
-
+	 
 
 exit:
 	la $a0, promptOutput					#print the total
 	li $v0, 4						#syscall that print
 	syscall
 	
-	addi $v0, $t1, 0					#save it into $v0
+	addi $v0, $s7, 0					#save it into $v0
 	addi $a0, $v0, 0					#save it into $a0
 	li $v0, 1						#print the sum
 	syscall
